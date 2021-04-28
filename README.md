9580e49c74b98b95693db4b55c541ef994a928c0
Jorge Hernandez <jorgehdzg1991@gmail.com>
2021-04-28 15:51:03 -0500

    se rescataron los archivos desde un stash

---
+++ README.md
@@ -0,0 +1,4 @@
+# learning-git
+Repo for demonstrating some of the concepts of Git
+
+Hello world!
---
+++ operaciones.js
@@ -0,0 +1,7 @@
+export const suma = (x, y) => x + y;
+
+export const resta = (x, y) => x - y;
+
+export const multiplicacion = (x, y) => x * y;
+
+export const division = (x, y) => x / y;
